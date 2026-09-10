185de222d04e0cd3b9e0d9336dc51e23d31d9e2f
darthwallace <wallaceleite915@gmail.com>
2026-09-10 17:04:03 -0300

    Adicionando o cadastro do usuário

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,8 +1,33 @@
-class CreateUserService {
-  async execute() {
-    console.log("EXECUTANDO SERVIÇOOOOO");
+import prismaClient from '../../prisma/index'
 
-    return "USUARIO MATHEUS CRIADO!!";
+interface CreateUserProps {
+  name: string;
+  email: string;
+  password: string;
+}
+
+
+class CreateUserService {
+  async execute({ name, email, password}: CreateUserProps) {
+
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+    }})
+
+    if (userAlreadyExists) {
+      throw new Error("usuario já cadastrado")
+    }
+
+    const user = await prismaClient.user.create({
+      data:{
+        name: name,
+        email: email,
+        password: password,
+      }
+    })
+
+    return user.name;
   }
 }
 

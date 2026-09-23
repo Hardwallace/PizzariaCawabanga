d641e7e4b19cfbc9a426c0f258a98ba48e03943d
darthwallace <wallaceleite915@gmail.com>
2026-09-23 18:13:19 -0300

    Adicionando a lista de categorias cadastradas

--- src/routes.ts
+++ src/routes.ts
@@ -1,23 +1,40 @@
 import { Router } from "express";
-import { CreateUserController } from "./controllers/user/CreateUserController";
-import { validateSchema } from "./middlewares/validateSchema";
-import { createUserSchema, authUserSchema } from "./schemas/userSchema";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
 import { AuthUserController } from "./controllers/user/authUserController";
+import { CreateUserController } from "./controllers/user/CreateUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
-import { IsAuthenticated } from "./middlewares/IsAuthenticated";
-import { CreateCategoryController } from "./controllers/category/CreateCategoryController"
 import { IsAdmin } from "./middlewares/IsAdmin";
-import { createCategorySchema } from "./schemas/categorySchema"; 
-
+import { IsAuthenticated } from "./middlewares/IsAuthenticated";
+import { validateSchema } from "./middlewares/validateSchema";
+import { createCategorySchema } from "./schemas/categorySchema";
+import { authUserSchema, createUserSchema } from "./schemas/userSchema";
 
 const router = Router();
 
-router.post("/users", validateSchema(createUserSchema), new CreateUserController().handle);
+router.post(
+  "/users",
+  validateSchema(createUserSchema),
+  new CreateUserController().handle
+);
 
-router.post("/session" , validateSchema(authUserSchema), new AuthUserController().handle);
+router.post(
+  "/session",
+  validateSchema(authUserSchema),
+  new AuthUserController().handle
+);
 
-router.post("/category" , IsAuthenticated , IsAdmin , validateSchema(createCategorySchema), new CreateCategoryController().handle);
+router.get("/me", IsAuthenticated, new DetailUserController().handle);
 
-router.get("/me" , IsAuthenticated , new DetailUserController().handle);
+
+router.post(
+  "/category",
+  IsAuthenticated,
+  IsAdmin,
+  validateSchema(createCategorySchema),
+  new CreateCategoryController().handle
+);
+
+router.get("/category", IsAuthenticated, new ListCategoryController().handle);
 
 export { router };
